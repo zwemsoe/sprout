a0c9818dc3f5e0d48b8c96302357a33e53ce6f59
zsoe <zwemsoe@gmail.com>
2022-02-19 12:54:13 -0800

    socket

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,3 +1,19 @@
+import React, { useEffect, useContext } from "react";
+import { SocketContext } from "../contexts/socket";
+
 export default function SignUpPage() {
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.emit("user_join");
+    console.log("wat");
+  }, []);
+
+  useEffect(() => {
+    socket.on("join", () => {
+      console.log("joined!");
+    });
+  }, []);
+
   return <h1>Sign up page</h1>;
 }
