b6fa2cff4e52c338810ab48f377f6294702468f2
zsoe <zwemsoe@gmail.com>
2022-02-20 01:36:13 -0800

    gg

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -8,3 +8,8 @@ export const server_url =
   process.env.NODE_ENV === "production"
     ? "https://accessibility-passport.herokuapp.com"
     : "http://localhost:5000";
+
+export const client_url =
+  process.env.NODE_ENV === "production"
+    ? "https://accessibility-passport.netlify.app"
+    : "http://localhost:3000";
--- frontend/src/pages/QRCodePage.jsx
+++ frontend/src/pages/QRCodePage.jsx
@@ -13,6 +13,7 @@ import { useContext, useEffect } from "react";
 import { SocketContext } from "../contexts/socket";
 import { getLocalStorage } from "../utils/localstorage";
 import { useNavigate } from "react-router-dom";
+import { client_url } from "../constants";
 
 export default function QRCodePage() {
   const user = getLocalStorage("user");
@@ -29,27 +30,27 @@ export default function QRCodePage() {
   }, []);
 
   return (
-    <div id="doqr">
-      <Flex minHeight="100vh" align="center" justify="center">
-        <Stack direction="column" width={300}>
+    <div id='doqr'>
+      <Flex minHeight='100vh' align='center' justify='center'>
+        <Stack direction='column' width={300}>
           <Center>
             <Wrap>
               <WrapItem>
                 <Avatar
                   name={user.name}
                   src={`https://avatars.dicebear.com/api/jdenticon/${user.id}.svg`}
-                  size="2xl"
+                  size='2xl'
                 />{" "}
               </WrapItem>
             </Wrap>
           </Center>
           <Center pt={5}>
-            <Heading fontSize="3xl" fontWeight={800}>
+            <Heading fontSize='3xl' fontWeight={800}>
               {" "}
               Hello {`${user.name}`},
             </Heading>
           </Center>
-          <Center pt={5} textAlign="center">
+          <Center pt={5} textAlign='center'>
             <Text>
               Your QR code is ready to be used
               <br />
@@ -57,8 +58,8 @@ export default function QRCodePage() {
             </Text>
           </Center>
           <Center pt={50}>
-            <Box maxW="sm" borderWidth="10px" borderRadius="md" overflow="show">
-              <QRCode value={`http://localhost:3000/${user.id}`} />
+            <Box maxW='sm' borderWidth='10px' borderRadius='md' overflow='show'>
+              <QRCode value={`${client_url}/${user.id}`} />
             </Box>
           </Center>
         </Stack>
