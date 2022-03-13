5c8ea8b0c52237c0a26cc87938c95f9af1e20e6e
Alice Kang <akang28@students.cs.ubc.ca>
2022-03-13 00:16:46 -0800

    Sign up page with navigation

--- frontend/app/routes/AuthStack.js
+++ frontend/app/routes/AuthStack.js
@@ -1,5 +1,9 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import SignUp from "../screens/SignUp";
+import ScanPage from "../screens/user/ScanPage";
+import ActionsPage from "../screens/user/ActionPage";
+import QRCodePage from "../screens/restaurant/QRCode";
+import Room from "../screens/restaurant/Room";
 
 const Stack = createStackNavigator();
 
@@ -7,6 +11,10 @@ export default function AuthStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="ScanPage" component={ScanPage} />
+      <Stack.Screen name="ActionsPage" component={ActionsPage} />
+      <Stack.Screen name="QRCodePage" component={QRCodePage} />
+      <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
   );
 }
