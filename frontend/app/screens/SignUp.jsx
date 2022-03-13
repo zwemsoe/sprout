6552893beaead0a16324e0f8c53662c77603baa8
alice <alicekanng@gmail.com>
2022-03-12 21:40:42 -0700

    checkboxes

--- frontend/app/screens/SignUp.jsx
+++ frontend/app/screens/SignUp.jsx
@@ -1,6 +1,7 @@
 // Signup as either restaurant or user
 import { StyleSheet } from "react-native";
-import { Box, Button } from "native-base";
+import { Box, Button, FormControl, VStack, Input, Checkbox } from "native-base";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,12 +12,33 @@ const styles = StyleSheet.create({
 
 export default function SignUp({ navigation }) {
   const pressHandler = () => {
-    navigation.push('Scan Page')
-  }
+    navigation.push("Scan Page");
+  };
+
+  const [user, setUser] = useState();
+  const [restaurant, setRestaurant] = useState();
 
   return (
     <Box style={styles.container}>
-      <Button onPress={pressHandler}>Sign Up as Customer</Button>
+      <FormControl isRequired>
+        <VStack space={3}>
+          <FormControl.Label>Full Name</FormControl.Label>
+          <Input placeHolder="Name" />
+          <FormControl.HelperText>Enter your full name.</FormControl.HelperText>
+          <Checkbox.Group>
+            <Checkbox value="Hearing">
+              Hearing Impairment
+            </Checkbox>
+            <Checkbox value="Speech">
+              Speech Impairment
+            </Checkbox>
+            <Checkbox value="Motor">
+              Motor Impairment
+            </Checkbox>
+          </Checkbox.Group>
+          <Button onPress={pressHandler}>Sign Up as Customer</Button>
+        </VStack>
+      </FormControl>
       <Button>Sign Up as Restaurant</Button>
     </Box>
   );
