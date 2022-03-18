c7b505552e24aa51bff99f4033079b60435b128a
zwe <zwemsoe@gmail.com>
2022-03-18 13:54:38 -0700

    resturant pages

--- frontend/app/screens/restaurant/IndividualUser.jsx
+++ frontend/app/screens/restaurant/IndividualUser.jsx
@@ -10,7 +10,7 @@ import {
   Button,
   ScrollView,
   Center,
-  useTheme,
+  ArrowBackIcon,
 } from "native-base";
 
 const styles = StyleSheet.create({
@@ -18,33 +18,38 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     display: "flex",
-    paddingTop: 80,
+    paddingTop: 40,
   },
 });
 
+const Event = ({ type, message, timestamp }) => {
+  return (
+    <Box p={12} rounded="lg" bgColor="blueGray.500" w="100%">
+      Event: {`${type}`}
+    </Box>
+  );
+};
+
 export default function IndividualUserPage() {
-  const { colors } = useTheme();
   return (
     <Box style={styles.container}>
-      <Box
-        minW='90%'
-        p='5'
-        borderColor='coolGray.200'
-        bgColor='blue.400'
-        minH='120'
-      >
+      <Box ml={3}>
+        <ArrowBackIcon size="10" />
+      </Box>
+      <Box p="5" borderColor="coolGray.200" bgColor="blue.400" minH="120">
         <Box
           style={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-around",
+            alignItems: "center",
           }}
         >
           <Avatar
             source={{
               uri: "https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
             }}
-            size='lg'
+            size="lg"
           />
           <VStack>
             <Heading>Name</Heading>
@@ -53,66 +58,22 @@ export default function IndividualUserPage() {
         </Box>
       </Box>
       <Center style={{ marginTop: 10 }}>
+        <Heading>User Events:</Heading>
         <ScrollView
-          minW='95%'
-          h='80'
+          h={600}
+          pt={5}
           _contentContainerStyle={{
             px: "20px",
             mb: "4",
-            minW: "72",
+            minW: "100%",
           }}
         >
-          <VStack flex='1'>
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
-          </VStack>
-          <VStack flex='1'>
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
-            {Object.keys(colors.cyan).map((key, index) => {
-              if (index >= 1 && index <= 5)
-                return (
-                  <Center py='4' bg={`cyan.${key}`}>
-                    {key}
-                  </Center>
-                );
-            })}
+          <VStack flex="1">
+            {[...Array(20).keys()].map((key, index) => (
+              <Center py="4">
+                <Event type="PAY_BILL" />
+              </Center>
+            ))}
           </VStack>
         </ScrollView>
       </Center>
